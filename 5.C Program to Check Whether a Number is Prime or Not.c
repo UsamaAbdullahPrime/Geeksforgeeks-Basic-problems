088256c4fd0cead1fa6d7dba91fcb8b9e5a118c6
Usama Abdullah <140549638+UsamaAbdullahPrime@users.noreply.github.com>
2024-01-02 17:46:40 +0600

    Update 5.C Program to Check Whether a Number is Prime or Not.c

--- 5.C Program to Check Whether a Number is Prime or Not.c
+++ 5.C Program to Check Whether a Number is Prime or Not.c
@@ -1,37 +1,28 @@
-#include <stdio.h>
-
-int main()
-{   /*
-    To find a Prime number we can devide it by
-    two if theres remainder 0 then its not prime
-    but for two it can be derivable by 2 and remainder is 0
-    but it is a prime number
-    */
-    int number,i=2;
-    printf("Input a number to see if its prime or not:");
+#include<stdio.h>
+int main(){
+    int number,count = 0;
+    printf("Input to find either it's prime or not > ");
     scanf("%d",&number);
-    while(i<number && i<=2){
-    if(number==2)
-    {
-        printf("It is a prime number");
-        break;
+    if(number == 1 || number == 0){
+        printf("Not a Prime number");
+        count++;
     }
-    else if(number%2==0)
-    {
-        printf("It's not a prime number ");
-        break;
-
-    }
-    else if (number%2!=0)
-    {
-        printf("It's a prime number");
-        break;
+    else if(number == 2){
+        printf("It's a Prime number");
+        count++;
     }
     else
     {
-        printf("Wrong input");
-        break;
+        for(int i = 2;i<number;i++){
+           if (number % i == 0){
+                printf("Not a Prime number");
+                count++;
+                break;
+           }
+        }
     }
+
+    if(count==0){
+       printf("It's a Prime number");
     }
-    return 0;
 }
